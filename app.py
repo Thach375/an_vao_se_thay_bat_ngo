a5f174ea3e067b5fca24427035bd808cfc3c2e67
Thach375 <phamthach03072005@gmail.com>
2024-11-26 15:04:21 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/yes1")
+def yes1():
+    return render_template("yes1.html")
+
+@app.route("/no1")
+def no1():
+    return render_template("no1.html")
+
+@app.route("/yes2")
+def yes2():
+    return render_template("yes2.html")
+
+@app.route("/no2")
+def no2():
+    return render_template("no2.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
